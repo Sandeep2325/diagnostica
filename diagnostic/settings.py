2993be16527750ba5c710da738a96f52c0bea769
sandeep2315 <sandeepgowda2314@gmail.com>
2022-08-09 11:52:51 +0530

    tests-retrieve

--- diagnostic/settings.py
+++ diagnostic/settings.py
@@ -95,16 +95,16 @@ AUTH_USER_MODEL = 'app1.User'
 #     }
 # }
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'spandiagnodb',  
-        'USER': 'spandiagno_user',  
-        'PASSWORD': 'spanDiagnoV2db',  
-        'HOST': 'spandiagnov2.cubknyrg0xrn.us-east-2.rds.amazonaws.com',  
-        'PORT': '3306',   
-    }  
-} 
+# DATABASES = {  
+#     'default': {  
+#         'ENGINE': 'django.db.backends.mysql',  
+#         'NAME': 'spandiagnodb',  
+#         'USER': 'spandiagno_user',  
+#         'PASSWORD': 'spanDiagnoV2db',  
+#         'HOST': 'spandiagnov2.cubknyrg0xrn.us-east-2.rds.amazonaws.com',  
+#         'PORT': '3306',   
+#     }  
+# } 
 # DATABASES = {  
 #     'default': {  
 #         'ENGINE': 'django.db.backends.mysql',  
@@ -126,25 +126,25 @@ DATABASES = {
 #         'PORT': '3306',   
 #     }  
 # }
-# DATABASES = {  
+DATABASES = {  
 
-#     'default': {  
+    'default': {  
 
-#         'ENGINE': 'django.db.backends.mysql',  
+        'ENGINE': 'django.db.backends.mysql',  
 
-#         'NAME': 'diagnospan',  
+        'NAME': 'diagnospan',  
 
-#         'USER': 'root',  
+        'USER': 'root',  
 
-#         'PASSWORD': 'mySqlServer@#$432',  
+        'PASSWORD': 'mySqlServer@#$432',  
 
-#         'HOST': 'localhost',  
+        'HOST': 'localhost',  
 
-#         'PORT': '3306',   
+        'PORT': '3306',   
 
-#     }  
+    }  
 
-# }
+}
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 20000
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
 # Password validation
@@ -194,13 +194,13 @@ STATIC_URL = '/static/'
 RAZOR_KEY_ID = "rzp_test_JiD8eNtJ2aNwZr"
 RAZOR_KEY_SECRET = "gtukARkLZ5U4Bjo9EfCSWkMf"
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# # EMAIL_USE_SSL=FALSE
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = "gowdasandeep8105@gmail.com"
-# EMAIL_HOST_PASSWORD = 'atkzlpfgzcvpdhai'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL=FALSE
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "gowdasandeep8105@gmail.com"
+EMAIL_HOST_PASSWORD = 'atkzlpfgzcvpdhai'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'mail.spandiagno.com'
@@ -209,13 +209,13 @@ RAZOR_KEY_SECRET = "gtukARkLZ5U4Bjo9EfCSWkMf"
 # EMAIL_HOST_USER = "donotreplay@spandiagno.com"
 # EMAIL_HOST_PASSWORD = 'Fullmoon22@'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_USE_TLS = True
-# EMAIL_USE_SSL=FALSE
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "enquiry@spanhealth.com"
-EMAIL_HOST_PASSWORD = 'Ravi@123'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp-mail.outlook.com'
+# EMAIL_USE_TLS = True
+# # EMAIL_USE_SSL=FALSE
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "enquiry@spanhealth.com"
+# EMAIL_HOST_PASSWORD = 'Ravi@123'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SESSION_SAVE_EVERY_REQUEST =True
