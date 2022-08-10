cba7d8aa7af294a9965f70d95b16a27d228475ac
sandeep2315 <sandeepgowda2314@gmail.com>
2022-08-10 11:24:11 +0530

    serverpush

--- diagnostic/settings.py
+++ diagnostic/settings.py
@@ -194,13 +194,13 @@ STATIC_URL = '/static/'
 RAZOR_KEY_ID = "rzp_test_JiD8eNtJ2aNwZr"
 RAZOR_KEY_SECRET = "gtukARkLZ5U4Bjo9EfCSWkMf"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-# EMAIL_USE_SSL=FALSE
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "gowdasandeep8105@gmail.com"
-EMAIL_HOST_PASSWORD = 'atkzlpfgzcvpdhai'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# # EMAIL_USE_SSL=FALSE
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "gowdasandeep8105@gmail.com"
+# EMAIL_HOST_PASSWORD = 'atkzlpfgzcvpdhai'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'mail.spandiagno.com'
