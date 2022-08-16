65a07bdb406ba2ff3e2b0870b4356f3ec98907b6
shivraj <shivraj.nexevo@gmail.com>
2022-08-16 15:24:35 +0530

    shivraj phase2 16/8/2022

--- diagnostic/settings.py
+++ diagnostic/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     
 ]
 INSTALLED_APPS += ('django_summernote', ) 
-
+INSTALLED_APPS += ['django_social_share']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
